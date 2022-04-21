926025979db3e3f28353be4754bb2d5ed42506e7
TariqHusainKhan <61875008+TariqHusainKhan@users.noreply.github.com>
2022-04-22 03:16:34 +0530

    middleware1 assignment added

--- src/index.js
+++ src/index.js
@@ -8,20 +8,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+const midGlb = function(req,res,next)
+{
+    console.log("Hi, I am in the global Miiddleware");
+  next();
+}
+
+
+const assignmentMw = function (req,res,next)
+{
+    var currentDate = new Date();
+    var dateTime =currentDate.getDate()+" "
+                  + (currentDate.getMonth()+1) + " "
+                  +currentDate.getFullYear()+ " "
+                  +currentDate.getHours()+ ": "
+                  +currentDate.getMinutes()+ " "
+                  currentDate.getSeconds();
+
+                  let ip =req.ip;
+                  let url = req.originalUrl;
+                  console.log(`${dateTime} ${ip} ${url}`);
+                  next();
+}
+app.use(assignmentMw);
+
+
+mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/TariqHusainKhanDB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
 
-app.use('/', route);
+app.use('/', route)
 
 
 app.listen(process.env.PORT || 3000, function () {
