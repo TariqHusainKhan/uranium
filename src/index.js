7c80366a83bf061df406705cd310dee153ebf042
TariqHusainKhan <61875008+TariqHusainKhan@users.noreply.github.com>
2022-04-24 23:07:44 +0530

    21st april assignment related to Middleware

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort580:WwiSjumsqAyT4pPC@cluster0.hxlgz.mongodb.net/TariqHusainKhanDB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/models/orderModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const orderSchema = new mongoose.Schema( {
+    userId: ObjectId,
+    productId: ObjectId,
+    amount: Number,
+    isFreeAppUser: Boolean,
+    date: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('middlewareOrders', orderSchema) //order 
