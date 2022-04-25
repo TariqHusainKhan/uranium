243e386ce0f12b814e09fe88250d52e9357b886a
TariqHusainKhan <61875008+TariqHusainKhan@users.noreply.github.com>
2022-04-25 07:00:14 +0530

    assignment of authentication

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema( {
     firstName: String,
     lastName: String,
     mobile: {
         type: String,
-
         required: true
     },
     emailId: String,
@@ -15,6 +13,7 @@ const userSchema = new mongoose.Schema( {
         enum: ["male", "female", "other"]
     },
     age: Number,
+    isDeleted : { type: Boolean, default: false}
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('mw2User', userSchema)
